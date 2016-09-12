384ab1d0ccc78a1bc40e2f56c339dd0272b4d8af
BT-Training <bt-training@bt-trainings-mac.local>
2016-09-12 09:31:46 +0200

    Initial Commit

---
+++ ProductionCost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProductionCost
+//
+//  Created by BT-Training on 12/09/16.
+//  Copyright © 2016 BT-Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
